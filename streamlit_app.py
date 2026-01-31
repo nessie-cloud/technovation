3e4656c94e8a5426db7a3893cdd5cadb5bbfe59a
nessie-cloud <kaadnessie@gmail.com>
2026-01-30 21:10:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
+st.title("GATITOS BEBES")
+st.write("FUNDACION PARA ALIMENTAR GATITOS BEBES")
+ST.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRmMrxgGei1NFtFBW40yEnWcSOhDihDDgfSw&s")
+
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
